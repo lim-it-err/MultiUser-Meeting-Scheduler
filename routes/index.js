f8a379b49029f99ca2740c5be21e684aedf73e90
JungHyeokAhn <yehsi2000@gmail.com>
2022-11-27 01:09:07 +0900

    Added db data fetching example
    간단히 테스트할수있도록 요청 두개 추가
    GET /testcreate : 테스트용 data추가
    GET /user/schedule/getusertime : id가 test인 유저의 usertime 모두 가져오는 테스트 요청

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const models = require('../models');
 
 router.get('/', (req, res, next) => {
   res.redirect('/login');
@@ -8,5 +9,25 @@ router.get('/', (req, res, next) => {
 router.get('/login', (req, res, next) => {
   res.render("login.html");
 });
+
+router.get('/testcreate', async(req, res, next) => {
+    const user = await models.User.create({
+      uid : "test",
+      password :1234,
+      name : "테스트유저"
+    });
+    const schedule = await models.Schedule.create({
+      name : "테스트일정",
+      sched_day : new Date(),
+      uid : user.uid,
+    });
+    const usertime = await models.UserTime.create({
+      start_time : new Date(),
+      end_time : new Date("2022-11-27T02:00:00"),
+      schedule_id : schedule.schedule_id,
+      uid : user.id
+    });
+    res.sendStatus(200);
+})
 
 module.exports = router;
--- routes/user/index.js
+++ routes/user/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const models = require('../../models');
 
 router.get('/', (req, res, next) => {
   res.redirect('/user/main');
@@ -8,14 +9,24 @@ router.get('/', (req, res, next) => {
 router.get('/main', (req,res,next)=>{
   let a = [1,2,3]; //이런식으로 페이지에 변수전달가능
   res.render("user/main.html",a);
-})
+});
 
 router.get('/schedule', (req,res,next)=>{
   res.render("user/schedule.html");
-})
+});
 
 router.get('/make_schedule', (req,res,next)=>{
   res.render('user/make_schedule.html');
+});
+
+router.get('/schedule/getusertime',async(req,res,next)=>{
+  const usertime = await models.UserTime.findAll({
+    where: {
+      uid: "test"
+    },
+    include: [models.User, models.Schedule]
+  });
+  res.send(usertime);
 })
 
 module.exports = router;
