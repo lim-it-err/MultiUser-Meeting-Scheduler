ca3428ef8de0f28549bbd0a1c8aa63ef3a5e41dd
JH Ahn <39113797+yehsi2000@users.noreply.github.com>
2022-12-02 16:59:50 +0900

    Revert "Feat : Login ~ Make Schedule"

--- index.js
+++ index.js
@@ -1,68 +1,25 @@
 const express    = require('express');
-const path = require("path");
-const bodyParser = require('body-parser');
-const morgan = require("morgan");
-const models = require('./models');
-const cookieParser = require("cookie-parser");
+const mysql      = require('mysql');
+const dbconfig   = require('./config/database.js');
+const connection = mysql.createConnection(dbconfig);
 
 const app = express();
 
 // configuration =========================
-
-//view engine setting
-app.set('view engine', 'ejs');
-app.engine('html',require('ejs').renderFile);
-
-
-//middlewares
-if(process.env.NODE_ENV === "production") {
-  app.use(morgan('combined'));
-} else {
-  app.use(morgan('dev'));
-}
-
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-
-//port setting | default 3000
 app.set('port', process.env.PORT || 3000);
 
-//routing setting
-app.use('/',require('./routes'));
-app.use('/user',require('./routes/user'));
-app.use('/user/schedule',require('./routes/user/schedule'));
-app.use('/user/make_schedule',require('./routes/user/make_schedule'));
-
-//static file serving
-app.use('/',express.static(path.join(__dirname,"public")));
-
-//error handling
-app.use((req, res, next) => {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('/', (req, res) => {
+    res.send('Root');
 });
 
-
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  if (err.status == 404) {
-      res.send('Page Not Found');
-  } else {
-      res.send(`Uncaught error ${err.status}`);
-  }
-});
-
-models.sequelize.sync({force:false}) //true로 설정시 db모두 삭제후 재생성
-  .then(() => {
-    app.listen(app.get('port'), () => {
-      console.log('Express server listening on port ' + app.get('port'));
+app.get('/user', (req, res) => {
+    connection.query('SELECT * from user', (error, rows) => {
+        if (error) throw error;
+        console.log('User info is: ', rows);
+        res.send(rows);
     });
-  })
-  .catch(err => {
-    console.log("DB connection failed: " + err);
-  })
+});
+
+app.listen(app.get('port'), () => {
+    console.log('Express server listening on port ' + app.get('port'));
+});
